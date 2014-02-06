3110f7ab0b14e4b40a29eb643ec2604587de70d0
Dzikri Aziz <kvcrvt@gmail.com>
2014-02-06 11:22:24 +0700

    WP Theme: Add Writr 1.0.8

--- composer/src/includes/wordpress-themes.php
+++ composer/src/includes/wordpress-themes.php
@@ -3,6 +3,27 @@
 return array(
 	'packages' => array(
 		'kucrut/writr' => array(
+			'1.0.8' => array(
+				'name'    => 'kucrut/writr',
+				'version' => '1.0.8',
+				'dist'    => array(
+					'type' => 'zip',
+					'url'  => 'http://wordpress.org/themes/download/writr.1.0.8.zip',
+				),
+				'require' => array(
+					'kucrut/wordpress' => '~3.8',
+				),
+				'type'    => 'wordpress-theme',
+				'license' => array(
+					'GPL-2.0+',
+				),
+				'authors' => array(
+					array(
+						'name'     => 'Automattic',
+						'homepage' => 'http://automattic.com/',
+					),
+				),
+			),
 			'1.0.7' => array(
 				'name'    => 'kucrut/writr',
 				'version' => '1.0.7',
