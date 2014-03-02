46a3d0e60fb1a89adbf0ab3f09eeff7a8ef1ad23
Dzikri Aziz <kvcrvt@gmail.com>
2014-03-02 21:48:22 +0700

    WP Themes: Add "Hex" by Automattic

--- composer/src/includes/wordpress-themes.php
+++ composer/src/includes/wordpress-themes.php
@@ -46,5 +46,28 @@ return array(
 				),
 			),
 		),
+		'kucrut/hex' => array(
+			'1.0.1' => array(
+				'name'    => 'kucrut/writr',
+				'version' => '1.0.1',
+				'dist'    => array(
+					'type' => 'zip',
+					'url'  => 'http://wordpress.org/themes/download/hex.1.0.1.zip',
+				),
+				'require' => array(
+					'kucrut/wordpress' => '~3.8',
+				),
+				'type'    => 'wordpress-theme',
+				'license' => array(
+					'GPL-2.0+',
+				),
+				'authors' => array(
+					array(
+						'name'     => 'Automattic',
+						'homepage' => 'http://automattic.com/',
+					),
+				),
+			),
+		),
 	),
 );
