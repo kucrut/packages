796a2f035f750367f341fa2347f1c8edf287d3a2
Dzikri Aziz <kvcrvt@gmail.com>
2015-04-22 14:46:08 +0700

    Update WP: 4.1.2

--- composer/src/packages.php
+++ composer/src/packages.php
@@ -58,13 +58,13 @@ return array(
 			),
 		),
 		'kucrut/wordpress' => array(
-			'4.1.1' => array(
+			'4.1.2' => array(
 				'name'    => 'kucrut/wordpress',
-				'version' => '4.1.1',
+				'version' => '4.1.2',
 				'dist'    => array(
 					'type' => 'tar',
-					'url'  => 'http://wordpress.org/wordpress-4.1.1.tar.gz',
-					'sha1' => 'http://wordpress.org/wordpress-4.1.1.tar.gz.sha1',
+					'url'  => 'http://wordpress.org/wordpress-4.1.2.tar.gz',
+					'sha1' => 'http://wordpress.org/wordpress-4.1.2.tar.gz.sha1',
 				),
 				'require' => array(
 					'php' => '>=5.2.0',
