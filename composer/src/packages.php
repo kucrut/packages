5f901bd6a5e87659ab7f11443a889f6fd66a994d
Dzikri Aziz <kvcrvt@gmail.com>
2015-09-16 01:39:13 +0700

    WordPress 4.3.1

--- composer/src/packages.php
+++ composer/src/packages.php
@@ -58,13 +58,13 @@ return array(
 			),
 		),
 		'kucrut/wordpress' => array(
-			'4.3' => array(
+			'4.3.1' => array(
 				'name'    => 'kucrut/wordpress',
-				'version' => '4.3',
+				'version' => '4.3.1',
 				'dist'    => array(
 					'type' => 'tar',
-					'url'  => 'http://wordpress.org/wordpress-4.3.tar.gz',
-					'sha1' => 'http://wordpress.org/wordpress-4.3.tar.gz.sha1',
+					'url'  => 'http://wordpress.org/wordpress-4.3.1.tar.gz',
+					'sha1' => 'http://wordpress.org/wordpress-4.3.1.tar.gz.sha1',
 				),
 				'require' => array(
 					'php' => '>=5.2.0',
